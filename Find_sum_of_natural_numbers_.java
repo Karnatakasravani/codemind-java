9ff59cdcbd3779f9a9604c2c80fb3596ea781fef
Karnatakasravani <119839721+Karnatakasravani@users.noreply.github.com>
2023-08-18 11:46:57 +0530

    import java.util.Scanner; public class F{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int i;         int s=n*(n+1)/2;         //for(i=0;i<=n;i++)         //{
                System.out.print(s);
            //}
        }
    }

---
+++ Find_sum_of_natural_numbers_.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class F{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int i;
+        int s=n*(n+1)/2;
+        //for(i=0;i<=n;i++)
+        //{
+            
+            System.out.print(s);
+        //}
+    }
+}
