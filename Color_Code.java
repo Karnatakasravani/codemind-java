2aa780e4f255627e59487c1e5c1a012597caa58b
Karnatakasravani <119839721+Karnatakasravani@users.noreply.github.com>
2023-08-24 14:53:28 +0530

    import java.util.Scanner; public class ColorCode{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         char ch=sc.next().charAt(0);         if(ch == 'V' || ch =='v')         {             System.out.printf("Violet");         }         else if(ch =='I' ||  ch == 'i')         {             System.out.printf("Indigo");         }         else if(ch =='B'||ch =='b')         {             System.out.printf("Blue");         }         else if(ch == 'G' || ch == 'g' )         {             System.out.printf("Green");         }         else if(ch =='Y'||ch =='y')         {             System.out.printf("Yellow");         }         else if(ch =='O'||ch =='o')         {             System.out.printf("Orange");         }         else if(ch == 'R' || ch == 'r')         {             System.out.printf("Red");         }         else         {             System.out.println(-1);         }     } }

---
+++ Color_Code.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class ColorCode{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        char ch=sc.next().charAt(0);
+        if(ch == 'V' || ch =='v')
+        {
+            System.out.printf("Violet");
+        }
+        else if(ch =='I' ||  ch == 'i')
+        {
+            System.out.printf("Indigo");
+        }
+        else if(ch =='B'||ch =='b')
+        {
+            System.out.printf("Blue");
+        }
+        else if(ch == 'G' || ch == 'g' )
+        {
+            System.out.printf("Green");
+        }
+        else if(ch =='Y'||ch =='y')
+        {
+            System.out.printf("Yellow");
+        }
+        else if(ch =='O'||ch =='o')
+        {
+            System.out.printf("Orange");
+        }
+        else if(ch == 'R' || ch == 'r')
+        {
+            System.out.printf("Red");
+        }
+        else
+        {
+            System.out.println(-1);
+        }
+    }
+}
