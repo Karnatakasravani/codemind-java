10c3d81d61a3a05a33d37f6fdee3c8ff1105cb2b
Karnatakasravani <119839721+Karnatakasravani@users.noreply.github.com>
2023-09-14 19:25:40 +0530

    import java.util.Scanner; public class A {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int r,sum=0,temp;         temp=n;         while(n>0)         {             r=n%10;  //getting remainder             sum=(sum*10)+r;             n=n/10;         }         if(temp==sum)         System.out.println("True");         else         System.out.println("False");     } }

---
+++ Palindrome.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class A
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int r,sum=0,temp;
+        temp=n;
+        while(n>0)
+        {
+            r=n%10;  //getting remainder  
+            sum=(sum*10)+r;    
+            n=n/10;    
+        }    
+        if(temp==sum)    
+        System.out.println("True");    
+        else    
+        System.out.println("False");    
+    }
+}
